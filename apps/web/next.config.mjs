6056d532028adecd51944982a5c144b2865e3c02
zocomputer <help@zocomputer.com>
2026-06-14 15:35:07 +0000

    Fix deployment configuration for Cloudflare Pages

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    runtime: 'edge',
-  },
-};
+}
 
-export default nextConfig;
+export default nextConfig
